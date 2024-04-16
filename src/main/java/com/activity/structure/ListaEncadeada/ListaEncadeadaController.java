447f44eeba51cfc00fcc1d0b2f32d5fa7324ee41
joaoagr1 <joao.rollo1@gmail.com>
2024-04-16 20:18:43 -0300

    initial commit

---
+++ src/main/java/com/activity/structure/ListaEncadeada/ListaEncadeadaController.java
@@ -0,0 +1,59 @@
+package com.activity.structure.ListaEncadeada;
+
+import com.activity.structure.Contato.Contato;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/atividade-lista-encadeada")
+public class ListaEncadeadaController {
+
+    @Autowired
+    private ListaEncadeadaListaTelefonicaRepository listaTelefonicaRepository;
+    @Autowired
+    private EntityManager entityManager;
+
+    @PostMapping("/criar")
+    public ResponseEntity<ListaEncadeadaListaTelefonica> criarListaTelefonica() {
+        ListaEncadeadaListaTelefonica novaLista = new ListaEncadeadaListaTelefonica();
+        ListaEncadeadaListaTelefonica listaSalva = listaTelefonicaRepository.save(novaLista);
+        return ResponseEntity.ok().body(listaSalva);
+    }
+
+    @Transactional
+    @PostMapping("/{id}/adicionar-contato")
+    public ResponseEntity<ListaEncadeadaListaTelefonica> adicionarContato(@PathVariable Long id, @RequestBody Contato contato) {
+        ListaEncadeadaListaTelefonica lista = listaTelefonicaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Lista telefônica não encontrada"));
+        lista.adicionarContato(contato);
+        entityManager.persist(contato);
+        entityManager.persist(lista);
+        return ResponseEntity.ok().body(lista);
+    }
+
+    @Transactional
+    @DeleteMapping("/{id}/remover-contato")
+    public ResponseEntity<?> removerContato(@PathVariable Long id, @RequestParam String nome) {
+        ListaEncadeadaListaTelefonica lista = listaTelefonicaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Lista telefônica não encontrada"));
+        lista.removerContato(nome);
+        listaTelefonicaRepository.save(lista);
+        System.out.println(lista);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/buscar-contato")
+    public ResponseEntity<Contato> buscarContato(@PathVariable Long id, @RequestParam String nome) {
+        ListaEncadeadaListaTelefonica lista = listaTelefonicaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Lista telefônica não encontrada"));
+        Contato contato = lista.buscarContato(nome);
+        System.out.println(contato);
+        if (contato == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(contato);
+    }
+}
---
+++ src/main/java/com/activity/structure/ListaEncadeada/ListaEncadeadaListaTelefonicaRepository.java
@@ -0,0 +1,8 @@
+package com.activity.structure.ListaEncadeada;
+
+import com.activity.structure.ListaEncadeada.ListaEncadeadaListaTelefonica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListaEncadeadaListaTelefonicaRepository extends JpaRepository<ListaEncadeadaListaTelefonica, Long> {
+
+}
